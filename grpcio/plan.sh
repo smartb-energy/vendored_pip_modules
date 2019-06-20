c01e35f93f43ee3182a3c8882bfdd012495d432d
Blake Irvin <blake.irvin@smartb.eu>
2019-06-20 15:04:17 +0100

    add `grpcio` and `scikit-learn` modules

---
+++ grpcio/plan.sh
@@ -0,0 +1,5 @@
+pkg_origin=pip
+pkg_name=grpcio
+pkg_version="1.19.0"
+pkg_scaffolding="smartb/scaffolding-pip"
+scaffolding_python_pkg="core/python36"
---
+++ scikit-learn/plan.sh
@@ -0,0 +1,5 @@
+pkg_origin=pip
+pkg_name=scikit-learn
+pkg_version="0.20.3"
+pkg_scaffolding="smartb/scaffolding-pip"
+scaffolding_python_pkg="core/python36"
